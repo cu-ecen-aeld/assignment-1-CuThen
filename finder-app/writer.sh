20bcf2111e3cfe4c99027229489753be4451b953
sonphuc <sonphuc@Acer.myguest.virtualbox.org>
2024-08-12 17:18:02 +0700

    Finishing assignment 1 with full test successfully

---
+++ finder-app/writer.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+# A script for writer app.
+
+writefile=$1
+writestr=$2
+
+if [ -n "$writefile" ] ; then
+  echo "The file '$writefile' exists."
+else
+  echo "Error: Missing parameters. File directory $writefile is not defined"
+  exit 1
+fi
+
+if [ -n "$writestr" ]; then
+  echo "The writestr is '$writestr'."
+else
+  echo "Error: The writestr argument is empty."
+  exit 1
+fi
+
+#Writing into the new file
+echo "${writestr}" > ${writefile}
+
+if [ $? -ne 0 ]
+then
+        echo "Failed to write ${writefile} file"
+      	exit 1
+fi
+exit 0
